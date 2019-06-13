c26c51a81f3909e04c8ca20e0a455b9256c7fd01
greenhornsr <srollins11279@gmail.com>
2019-06-12 17:30:17 -0700

    refactored auth-mw to include ternary

--- auth/auth-mw.js
+++ auth/auth-mw.js
@@ -4,17 +4,16 @@ const secrets = require('../config/supasecrets');
 module.exports = (req,res, next) => {
     const token = req.headers.authorization;
 
-    if(token) {
-        jwt.verify(token, secrets.jwtSupaSecret, (err, decodedToken) => {
-            if(err) {
-                res.status(401).json({message: 'Invalid Credentials'})
-            }else{
-                const {subject, username, department} = decodedToken
-                req.activeUser = {id: subject, username: username, department: department}
-                next();
-            }
-        })
-    }else {
-        res.status(400).json({ message: 'No token provided' });
-    }
+    token ? 
+    jwt.verify(token, secrets.jwtSupaSecret, (err, decodedToken) => {
+        if(err) {
+            res.status(401).json({message: 'Invalid Credentials'})
+        }else{
+            const {subject, username, department} = decodedToken
+            req.activeUser = {id: subject, username: username, department: department}
+            next();
+        }
+    })
+    :
+    res.status(400).json({ message: 'No token provided' });
 }
