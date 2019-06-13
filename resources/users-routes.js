cc51944b4488c6887e5a875ccf57d218b2c50920
greenhornsr <srollins11279@gmail.com>
2019-06-12 17:11:48 -0700

    refactored get route to better reflect a check for ADMIN status or not

--- resources/users-routes.js
+++ resources/users-routes.js
@@ -3,25 +3,28 @@ const db = require('./users-model');
 const authmw = require('../auth/auth-mw');
 const depart = require('../auth/department-authmw');
 
+
 router.get('/', authmw, depart, (req, res) => {
     const {department} = req.activeUser
-    console.log(department)
+    department && department === 'ADMIN' ? 
     db.find()
     .then(users => {
-        users && req.activeUser.department === 'ADMIN' ? res.status(200).json({success: true, users}):
-        db.findByDepartment({department})
-        .then(sameDepartUsers => {
-            sameDepartUsers ? res.status(200).json({success: true, sameDepartUsers}):
-            res.status(400).json({success: false, message: 'sorry, no users matching your department.'})
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(403).json({message: 'no such luck!', err})
-        })
+        res.status(200).json({success: true, users})
     })
     .catch(err => {
-        res.status(500).json(errorRef(err))
+        console.log(err)
+        res.status(403).json({message: 'no such luck!', err})
     })
+    :
+    db.findByDepartment({department})
+    .then(sameDepartUsers => {
+        sameDepartUsers ? res.status(200).json({success: true, sameDepartUsers}):
+        res.status(400).json({success: false, message: 'sorry, no users matching your department.'})
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(errorRef(err))
+    })  
 })
 
 // Error Middleware
